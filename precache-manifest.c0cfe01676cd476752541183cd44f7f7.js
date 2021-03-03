616114bb35f52e4bee32cb4c3defa0f51e244b95
axeemhaider <axeemhaider@gmail.com>
2021-03-03 11:37:00 +0500

    Updates

---
+++ precache-manifest.c0cfe01676cd476752541183cd44f7f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfca8968f740c14ed9579276451da00f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61817504ea5acaeec18c",
+    "url": "/static/css/2.9d5983be.chunk.css"
+  },
+  {
+    "revision": "5e2fe4eed2db10213a3b",
+    "url": "/static/css/main.ed1f2490.chunk.css"
+  },
+  {
+    "revision": "61817504ea5acaeec18c",
+    "url": "/static/js/2.812942b1.chunk.js"
+  },
+  {
+    "revision": "c527f6856b108f91426fceea77d6000d",
+    "url": "/static/js/2.812942b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e2fe4eed2db10213a3b",
+    "url": "/static/js/main.f931f06f.chunk.js"
+  },
+  {
+    "revision": "b81ee488eb8cde580e1b",
+    "url": "/static/js/runtime-main.111cfa86.js"
+  }
+]);
